f26e0be6e5e4f581a996d308e3eb735475caa9dd
Joseph Rosenberg <jofus101@gmail.com>
2014-07-29 17:39:11 +0000

    Update the node engine version, create an index.js file that returns a 'hello world', and add port forwarding to the vagrantfile.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-hello world
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(5000);
