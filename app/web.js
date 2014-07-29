d2fa5bdde7bf0e2233c37d23402e784690f0a754
Joseph Rosenberg <jofus101@gmail.com>
2014-07-29 18:26:54 +0000

    Modify port listening and add npm version info

--- app/web.js
+++ app/web.js
@@ -1,7 +1,8 @@
 var http = require("http");
+var port = process.env.PORT || 8000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}).listen(port);
